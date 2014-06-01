9cb9e54e04d5d6249ffe4003e13d55aa5ed4b2c3
felipe.vasquez <felipe.vasquezm@alumnos.usm.cl>
2014-06-01 02:37:12 -0400

    modificaciones por tarea 2

--- Tarea1/src/peticionweb.java
+++ Tarea1/src/peticionweb.java
@@ -6,25 +6,24 @@ import org.apache.commons.io.IOUtils;
 
 class peticionWeb extends Thread
 {
+	private Socket s;
 	
-	final int DEBUG = 2;
-
-	
-	private Socket scliente = null;		
-   	private PrintWriter out = null;		
-	private BufferedReader in = null;
+	private  Socket scliente = null;		// representa la petición de nuestro cliente
+   	private  PrintWriter out = null;		// representa el buffer donde escribimos la respuesta
+	private  BufferedReader in = null;
 
    	peticionWeb(Socket ps)
    	{
 		scliente = ps;
    	}
 
-	public void run() 
+	public void run() // implementamos el metodo run
 	{
 		System.out.println("Procesamos conexion");
-
+		
 		try
 		{	
+			//s = new Socket("localhost",9999);
 			in = new BufferedReader (new InputStreamReader(scliente.getInputStream()));
   			out = new PrintWriter(new OutputStreamWriter(scliente.getOutputStream()),true) ;
   			        
@@ -36,43 +35,73 @@ class peticionWeb extends Thread
   				list.add(line);
   				System.out.println("--" + line + "-");
             	
-            	if(line.split("=")[0].equals("Nombre"))
-            	{
-            		agregarcontacto(line);
-            	}
             	if(line.isEmpty()){
 	            	cargarpagina(list); 
 	            }
             	
             }
+  			list.clear();
   			out.close();
-            list.clear();
-            
+                      
 		}
 		catch(Exception e)
 		{
 			System.out.println("Error en servidor\n" + e.toString());
 		}
-		
-		
+
+
 		System.out.println("Hemos terminado");
 	}
-	
-	
-	void cargarpagina(List<String> lista)
-	{
-		
+
+
+	void cargarpagina(List<String> lista) throws IOException
+	{		
+
 			String[] req = lista.get(0).split(" ");
-			
-			
 			String url = req[1];
 
-			if(url.equals("/inicio.html") || url.equals("/")){
+			if(req[0].equals("POST")){	
 				
+		    	  String datos;
+	              char[] aux = new char[100];
+	              in.read(aux);
+	              for (int i=0;i<100;i++)
+	                  if(aux[i]==(char)0)
+	                      aux[i]='*';
+	              datos=new String(aux);
+	              datos = datos.replace("*","");
+	              System.out.println("Datos: " + datos);
+	              
+	              if(url.equals("/inicio.html")){
+	            	  agregarcontacto(datos);
+	              }
+	              if(url.equals("/chat.html"))
+				  {
+	            	 //***AQUI TRABAJAR >:
+	            	  Socket s;
+	            	  s = new Socket("localhost",8080);
+	            	  PrintWriter enviar = new PrintWriter(new OutputStreamWriter(s.getOutputStream()),true);
+	            	  BufferedReader b = new BufferedReader ( new InputStreamReader ( s.getInputStream() ) );
+	            	  enviar.println( datos );
+	            	  
+	            	  
+	            	  String respuesta = b.readLine();
+	            	  System.out.println(respuesta);
+	            	  
+	            	  enviar.close();
+		  			  b.close();
+		  			  s.close();
+	            	  
+				  }
+	             
+	              
+		      }
+
+			if(url.equals("/inicio.html") || url.equals("/")){
 				File archivo = null;
 			    FileReader fr = null;
 			    BufferedReader br = null;
-			    
+
 			    try {                
 			    	archivo = new File("contactos.txt");
 			    	if ( !archivo.exists())
@@ -82,43 +111,46 @@ class peticionWeb extends Thread
 			    	}
 			    	fr = new FileReader(archivo);
 			    	br = new BufferedReader(fr);
-			    	
+
 			    	String linea;
 			    	String[] aux;
 			    	String nombre;
-			    	
+
+
+
 			    	out.println("<table>");
 			    	out.println("<tr><th scope='col'>Contactos</th>");
 			    	out.println("<th scope='col'>IP</th>");
 			    	out.println("<th scope='col'>Puerto</th></tr>");
-			    				    	
+
 			    	/*Lectura del archivo contactos.txt y escritura en el html*/
 			        while((linea=br.readLine())!=null)
 			        {	
 			        	aux = linea.split("&");
 			        	nombre = aux[0].split("=")[1].replace("+"," ");
-			        	
+
 			        	out.println("<tr>");			    			        	
 		        		out.println("<td>"+nombre+"</td>");
 		        		out.write("<td>"+ aux[1].split("=")[1] +"</td>\n");
 		                out.write("<td>"+ aux[2].split("=")[1] +"</td>\n");
 		        		out.println("</tr>");
-			        
-			        		
+
+
 			        } 
-			     	
+
 			        out.println("</table>");
-			     
+
 
 	    			InputStream a = new FileInputStream ("inicio.html");
 	                String html = IOUtils.toString(a, "UTF-8");
 	                out.println(html); 	
-	                
-		 
+	                out.close();
+
+
 		        } catch (Exception e) {
-		 
+
 		        	e.printStackTrace();
-		 
+
 		        }finally{
 		         try{                    
 		            if( null != fr ){   
@@ -130,32 +162,34 @@ class peticionWeb extends Thread
 		      }
  
 	    	}
-						
+
 			else if(req[1].equals("/agregarcontacto.html?")){
 				try
 	    		{	
-					
+
 					InputStream archivo = new FileInputStream ("agregarcontacto.html");
 	                String html = IOUtils.toString(archivo, "UTF-8");
-	                out.println(html);    
-	                
+	                out.println(html); 
+	                out.close();
+
 	    		}
-	    		
+
 	    		catch(Exception e)
 	    		{
-	    			System.out.println("Error en servidor\n" + e.toString());
+	    			System.out.println(e.toString());
 	    		}
-				
+
 			}
-			
-	      if(req[0].equals("POST")){
-	    		/**Aqui se supone que deberia estar la funcion agregarcontacto, la cual guarda el 
-	    		 * nuevo contacto en el .txt pero, por alguna razon guado se da click en el 
-	    		 * boton guardar contacto los datos no se leen y se queda como en espera... >:*/
-	  
-	    	}
+			if(url.equals("/chat.html")){
+				InputStream archivo = new FileInputStream ("chat.html");
+                String html = IOUtils.toString(archivo, "UTF-8");
+                out.println(html); 
+                out.close();
+			} 
+
+
 	}
-	
+
 	void agregarcontacto(String contacto){
 		FileWriter fichero = null;
         PrintWriter pw = null;
@@ -183,7 +217,7 @@ class peticionWeb extends Thread
            }
         }
 	}
-	
-	
-	
+
+
+
 }
--- Tarea1/src/webserver.java
+++ Tarea1/src/webserver.java
@@ -1,45 +1,66 @@
 
+import java.awt.Desktop;
+import java.io.IOException;
 import java.net.*;
 
 
 public class webserver
-{
+{	
 	int puerto = 8000;
 	private ServerSocket s;
-	
+
 	public static void main(String [] array) 	
 	{
 		webserver instancia = new webserver();	
 		instancia.arranca();
 		
-		
+
 	}
-	
+
 	boolean arranca()
 	{
 		System.out.println("Arrancamos nuestro servidor");
 		try
-		{	
-			s = new ServerSocket(puerto);
-			
+		{					
+			s = new ServerSocket(0);
+			System.out.println( "El sistema nos ha dado el puerto "+ s.getLocalPort());
 			System.out.println("Quedamos a la espera de conexion");
-						
+			String url = "http://localhost:"+ s.getLocalPort();
+			System.out.println(url);
+			if(Desktop.isDesktopSupported()){
+	            Desktop desktop = Desktop.getDesktop();
+	            try {
+	                desktop.browse(new URI(url));
+	            } catch (IOException | URISyntaxException e) {
+	                // TODO Auto-generated catch block
+	                e.printStackTrace();
+	            }
+	        }else{
+	            Runtime runtime = Runtime.getRuntime();
+	            try {
+	                runtime.exec("xdg-open " + url);
+	            } catch (IOException e) {
+	                // TODO Auto-generated catch block
+	                e.printStackTrace();
+	            }
+	        }
 			while(true)  
 			{
-				Socket entrante = s.accept();
+				Socket entrante = s.accept();		
+				
 				peticionWeb pCliente = new peticionWeb(entrante);
 				pCliente.start();
-				
+
 			}
-		
+
 		}
 		catch(Exception e)
 		{
-			System.out.println("Error en el servidor\n" + e.toString());
-					
+			System.out.println(e.toString());
+
 		}
-		
+
 		return true;
 	}
-	
+
 }
